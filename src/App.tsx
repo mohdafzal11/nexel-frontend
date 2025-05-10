aacfd191120594641ef6d2e9e598cae493f9dc52
Mohd Afzal <afzalnaved0000@gmail.com>
2025-05-10 15:20:36 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className='bg-black flex justify-center items-center h-screen'>
+        <h1 className='text-3xl font-bold underline text-blue-500'>Nexel</h1>
+    </div>
+  )
+}
+
+export default App
